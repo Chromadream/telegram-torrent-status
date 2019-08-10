4170e4a70ec72a922cb5cd89f8d143c365392388
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2019-08-10 20:31:15 +1000

    refactor: split test data

--- test/process/eta.test.ts
+++ test/process/eta.test.ts
@@ -2,56 +2,33 @@ import { describe, it } from "mocha";
 import { expect } from "chai";
 import { simpleTime } from "../../src/process";
 import { SimpleTime } from "../../src/model";
+import { ETAs } from "../testData/eta";
 
 describe("ETA processing", () => {
   it("should return correctly for <1m", done => {
     const actual = simpleTime(59);
-    const expected: SimpleTime = {
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 59,
-      total: 59,
-    };
+    const expected: SimpleTime = ETAs["1s"];
     expect(actual).to.deep.equals(expected);
     done();
   });
 
   it("should return correctly for >1min", done => {
     const actual = simpleTime(61);
-    const expected: SimpleTime = {
-      days: 0,
-      hours: 0,
-      minutes: 1,
-      seconds: 1,
-      total: 61,
-    };
+    const expected: SimpleTime = ETAs["1m"];
     expect(actual).to.deep.equal(expected);
     done();
   });
 
   it("should return correctly for >1hr", done => {
     const actual = simpleTime(3661);
-    const expected: SimpleTime = {
-      days: 0,
-      hours: 1,
-      minutes: 1,
-      seconds: 1,
-      total: 3661,
-    };
+    const expected: SimpleTime = ETAs["1h"];
     expect(actual).to.deep.equal(expected);
     done();
   });
 
   it("should return correctly for >1d", done => {
     const actual = simpleTime(86461);
-    const expected: SimpleTime = {
-      days: 1,
-      hours: 0,
-      minutes: 1,
-      seconds: 1,
-      total: 86461,
-    };
+    const expected: SimpleTime = ETAs["1d"];
     expect(actual).to.deep.equal(expected);
     done();
   });
